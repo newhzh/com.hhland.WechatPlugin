2307a7c2961c3f9f389dee3b1e21ce15b6758d7e
newhzh <newhzh@163.com>
2016-01-12 20:55:11 +0800

    android update2

--- www/WechatPlugin.js
+++ www/WechatPlugin.js
@@ -6,12 +6,12 @@ var exec = require('cordova/exec');
 var WechatPlugin=function(){};
 
 WechatPlugin.prototype.myAlert=function(){
-    alert('i from plugin');
+    alert('i from plugin,updated');
 };
 
 WechatPlugin.prototype.echo=function(onSuccess,onError,message){
     var params=[message];
-    exec(onSuccess,onError,'WechatPlugin','echo',params);
+    exec(onSuccess,onError,'WechatPlugin','echo',[]);
 };
 
 module.exports=new WechatPlugin();
