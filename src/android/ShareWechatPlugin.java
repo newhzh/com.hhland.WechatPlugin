c9bd7ce8d409c3256a581a2b41ac94a0473c4d73
newhzh <newhzh@163.com>
2016-01-14 22:36:13 +0800

    android add wx3

--- src/android/ShareWechatPlugin.java
+++ src/android/ShareWechatPlugin.java
@@ -1,4 +1,4 @@
-package com.hhland.cordova;
+package com.hhland.cordova.wx;
 
 import java.io.File;
 import java.io.IOException;
--- src/android/Util.java
+++ src/android/Util.java
@@ -1,4 +1,4 @@
-package com.hhland.cordova;
+package com.hhland.cordova.wx;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
