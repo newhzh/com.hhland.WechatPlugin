3a387fe76a1133a0d85ab297a1349f258d9bf763
newhzh <newhzh@163.com>
2016-08-17 12:11:01 +0800

    auth Update11

--- src/android/WXPayEntryActivity.java
+++ src/android/WXPayEntryActivity.java
@@ -16,14 +16,14 @@ public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler{
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		ShareWechatPlugin.api.handleIntent(getIntent(), this);
+		ShareWechatPlugin.instance.getWxAPI().handleIntent(getIntent(), this);
 	}
 	
 	@Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         setIntent(intent);
-        ShareWechatPlugin.api.handleIntent(intent, this);
+        ShareWechatPlugin.instance.getWxAPI().handleIntent(intent, this);
     }
 	
 	@Override
@@ -37,25 +37,25 @@ public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler{
 		// TODO Auto-generated method stub
 		switch(arg0.errCode){
 			case BaseResp.ErrCode.ERR_OK:
-	        	ShareWechatPlugin.currentCallbackContext.success(ShareWechatPlugin.SUCCESS);
+	        	ShareWechatPlugin.instance.getCurrentCallbackContext().success(ShareWechatPlugin.SUCCESS);
 	            break;
 	        case BaseResp.ErrCode.ERR_USER_CANCEL:
-	        	ShareWechatPlugin.currentCallbackContext.error(ShareWechatPlugin.ERR_USER_CANCEL);
+	        	ShareWechatPlugin.instance.getCurrentCallbackContext().error(ShareWechatPlugin.ERR_USER_CANCEL);
 	            break;
 	        case BaseResp.ErrCode.ERR_AUTH_DENIED:
-	        	ShareWechatPlugin.currentCallbackContext.error(ShareWechatPlugin.ERR_AUTH_DENIED);
+	        	ShareWechatPlugin.instance.getCurrentCallbackContext().error(ShareWechatPlugin.ERR_AUTH_DENIED);
 	            break;
 	        case BaseResp.ErrCode.ERR_SENT_FAILED:
-	        	ShareWechatPlugin.currentCallbackContext.error(ShareWechatPlugin.ERR_SENT_FAILED);
+	        	ShareWechatPlugin.instance.getCurrentCallbackContext().error(ShareWechatPlugin.ERR_SENT_FAILED);
 	            break;
 	        case BaseResp.ErrCode.ERR_UNSUPPORT:
-	        	ShareWechatPlugin.currentCallbackContext.error(ShareWechatPlugin.ERR_UNSUPPORT);
+	        	ShareWechatPlugin.instance.getCurrentCallbackContext().error(ShareWechatPlugin.ERR_UNSUPPORT);
 	            break;
 	        case BaseResp.ErrCode.ERR_COMM:
-	        	ShareWechatPlugin.currentCallbackContext.error(ShareWechatPlugin.ERR_COMM);
+	        	ShareWechatPlugin.instance.getCurrentCallbackContext().error(ShareWechatPlugin.ERR_COMM);
 	            break;
 	        default:
-	        	ShareWechatPlugin.currentCallbackContext.error(ShareWechatPlugin.ERR_UNKNOWN);
+	        	ShareWechatPlugin.instance.getCurrentCallbackContext().error(ShareWechatPlugin.ERR_UNKNOWN);
 	            break;
 		}
 		finish();
